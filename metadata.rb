18183f973c0a34082e97880ab41fdab36d7f5ef4
Victo Penso <vic.penso@gmail.com>
2013-04-09 17:31:16 +0200

    port of the server part from the original CMVFS cookbook

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Victor Penso"
+maintainer_email "vic.penso@gmail.com"
+license          "Apache 2.0"
+description      "Installs/Configures cvmfs"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+depends          "apache2"
+supports         "debian", ">= 7.0"
