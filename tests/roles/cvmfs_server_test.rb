1eac88ca73bea7e76c31f1fc8a2b92a1d5fdaa2a
Victor Penso <v.penso@gsi.de>
2013-11-11 18:15:41 +0100

    still working on remote publishing

--- tests/roles/cvmfs_server_test.rb
+++ tests/roles/cvmfs_server_test.rb
@@ -1,22 +1,13 @@
 name "cvmfs_server_test"
 description "Use to test the [cvmfs::server] recipe."
 run_list( 
+  "recipe[sys::pam]",
   "recipe[sys::ssh]",
   "recipe[cernvm-fs]" 
 )
 default_attributes(
   :sys => {
-    :ssh => {
-      :authorize => {
-        'cvmfs' => {
-          :keys => [ 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDZ33u0CFd+KbcT1agerP3zAbG57iXPKWT8ACznmWidHjAma23O8QvDXc3TuKu7OnsWpV6kCNpOJqw35pf7c6xRK+ZVWk8MnKkEm/Ht2UGukVWsLhlAPczG3Def8bkR0DrzOybR/ytWjnUbxGeg3EyJwgelA76KpNc/qdBeYyZI6/CgH3D0jJKfRdftv+4AC/tBBqHUeUa/RR7vG4yTasQ8HAIvCH4q5E3VO0HsWFZAxwodtOkmqaEmUoKxvXL1jCnttELzte1eQqMCkDtS4LhxfAeFGLwEDUTX+9eXstDkUY9BO0v1xDJnge5d0InVmBony1waXJU3Z7JTQTMj7aZ9' ]
-        }
-      }
-    }
+    :ssh => { :authorize => { 'cvmfs' => { :keys => [ 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDZ33u0CFd+KbcT1agerP3zAbG57iXPKWT8ACznmWidHjAma23O8QvDXc3TuKu7OnsWpV6kCNpOJqw35pf7c6xRK+ZVWk8MnKkEm/Ht2UGukVWsLhlAPczG3Def8bkR0DrzOybR/ytWjnUbxGeg3EyJwgelA76KpNc/qdBeYyZI6/CgH3D0jJKfRdftv+4AC/tBBqHUeUa/RR7vG4yTasQ8HAIvCH4q5E3VO0HsWFZAxwodtOkmqaEmUoKxvXL1jCnttELzte1eQqMCkDtS4LhxfAeFGLwEDUTX+9eXstDkUY9BO0v1xDJnge5d0InVmBony1waXJU3Z7JTQTMj7aZ9' ] } } }
   },
-  :cvmfs => {
-    :server => {
-      :repos => [ 'alpha.devops.test' ]
-    } 
-  }
+  :cvmfs => { :server => { :repos => [ 'alpha.devops.test' ] } }
 )
