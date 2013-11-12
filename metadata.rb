4646fe060c0fad061c7ac504754250d5c1a32576
Victor Penso <vic.penso@gmail.com>
2013-11-12 17:16:18 +0100

    fixing small issues

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
-maintainer       "Victor Penso"
-maintainer_email "vic.penso@gmail.com"
+maintainer       "GSI, HPC Department"
+maintainer_email "hpc@gsi.de"
 license          "Apache 2.0"
-description      "Deploy CernVM-FS clients and repository servers."
+description      "Deploya and configure  CernVM-FS clients and servers."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 depends          "sys"
