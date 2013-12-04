1067ed783aec2ee082b81830a30437bfe54c5b16
Victor Penso <v.penso@gsi.de>
2013-12-04 14:44:33 +0100

    apache needs to be a dependency to work with Chef server

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,5 @@ description      "Deploya and configure  CernVM-FS clients and servers."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 depends          "sys"
+depends          "apache2"
 supports         "debian", ">= 7.0"
