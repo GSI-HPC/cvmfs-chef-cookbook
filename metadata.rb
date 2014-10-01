56854ed57be836128813a11942e65a4a876eab77
Bastian Neuburger <b.neuburger@gsi.de>
2014-10-01 11:21:53 +0200

    deploy cronjob for auto resign on servers

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "hpc@gsi.de"
 license          "Apache 2.0"
 description      "Deploya and configure  CernVM-FS clients and servers."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
 depends          "sys"
 depends          "apache2"
 supports         "debian", ">= 7.0"
