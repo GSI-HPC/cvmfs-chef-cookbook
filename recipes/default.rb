2ff24e4af80f2a65fccec096eb8897aa5b7488a8
Christopher Huhn <C.Huhn@gsi.de>
2016-10-06 14:39:49 +0200

    Catch cvmfs.version == nil

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,7 +16,7 @@
 # limitations under the License.
 
 # Install from source if a specific version is defined
-unless node[:cvmfs][:version].empty?
+if node[:cvmfs][:version]
   include_recipe 'cernvm-fs::install'
 else
   # Otherwise it is assumed a package can be installed
