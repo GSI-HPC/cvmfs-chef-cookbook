b86fd9b3b9f863b594a901756b98fb0deb17c56d
Dennis Klein <d.klein@gsi.de>
2016-10-18 17:27:22 +0200

    fix bug

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "hpc@gsi.de"
 license          "Apache 2.0"
 description      "Deploy and configure CernVM-FS clients and servers."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.2.1"
 depends          "sys"
 depends          "apache2"
 supports         "debian", ">= 7.0"
