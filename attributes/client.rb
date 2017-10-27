fcaa514bbe447b68d43d26748536913dab6bd5d9
Matthias Pausch <m.pausch@gsi.de>
2017-10-27 11:41:45 +0200

    Fix some more depracation warnings

--- attributes/client.rb
+++ attributes/client.rb
@@ -1,4 +1,4 @@
-default[:cvmfs][:client][:default_local][:default_domain] = node.domain
+default[:cvmfs][:client][:default_local][:default_domain] = node['domain']
 default[:cvmfs][:client][:default_local][:cache_base] = '/var/cache/cvmfs'
 default[:cvmfs][:client][:default_local][:quota_limit] = 20000
 default[:cvmfs][:client][:default_local][:max_ttl] = 30
