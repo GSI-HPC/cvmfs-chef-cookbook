664c02304ecee2090755f8a817b7b646d0a1c7eb
Christopher Huhn <C.Huhn@gsi.de>
2019-09-23 16:26:41 +0200

    Pin apache2 cookbook to version 5 for Jessie compatibility

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ description      "Deploy and configure CernVM-FS clients and servers."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.2.3"
 depends          "sys"
-depends          "apache2"
+depends          "apache2", '< 6'
 supports         "debian", ">= 7.0"
