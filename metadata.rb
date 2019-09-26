183dd7b4e9b5170455c1dc45c7e5cbda12891181
Christopher Huhn <C.Huhn@gsi.de>
2019-09-26 15:43:50 +0200

    Adapt to new behaviour of sys::autofs

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "hpc@gsi.de"
 license          "Apache 2.0"
 description      "Deploy and configure CernVM-FS clients and servers."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.3"
-depends          "sys"
+version          "0.3.0"
+depends          "sys", '>= 1.51.0'
 depends          "apache2", '< 6'
 supports         "debian", ">= 7.0"
