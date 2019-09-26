74d183f3c49974ba35f8fdeab95104625face5e7
Christopher Huhn <C.Huhn@gsi.de>
2019-09-26 15:49:16 +0200

    Fix "ERROR: Chef::Exceptions::InvalidCookbookVersion: '6' does not match 'x.y.z' or 'x.y'"

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ description      "Deploy and configure CernVM-FS clients and servers."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.3.0"
 depends          "sys", '>= 1.51.0'
-depends          "apache2", '< 6'
+depends          "apache2", '< 6.0'
 supports         "debian", ">= 7.0"
