02e4d5fd59d3eab95b96bac6e2832adabaa12ef7
Christopher Huhn <C.Huhn@gsi.de>
2021-06-21 15:55:56 +0200

    Some more tests

--- test/integration/cernvm-fs/serverspec/localhost/default_spec.rb
+++ test/integration/cernvm-fs/serverspec/localhost/default_spec.rb
@@ -13,3 +13,21 @@ describe file('/cvmfs') do
   it { should exist }
   it { should be_directory }
 end
+
+describe command('cvmfs_config chksetup') do
+  its(:exit_status) { should be_zero }
+end
+
+describe command('automount -m') do
+  before do
+    `systemctl reload autofs.service`
+  end
+  its(:exit_status) { should be_zero }
+  its(:stderr) { should be_empty }
+  its(:stdout) { should contain "Mount point: /cvmfs" }
+  its(:stdout) { should contain "map: /etc/auto.cvmfs" }
+end
+
+describe file('/cvmfs/cvmfs-config.cern.ch') do
+  it { should be_directory }
+end
