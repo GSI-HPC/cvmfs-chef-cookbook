5be72696035caaeb2e2c5166b5a4d53242765d64
Christopher Huhn <C.Huhn@gsi.de>
2021-06-21 14:00:24 +0200

    Test right file, check /cvmfs

--- test/integration/cernvm-fs/serverspec/localhost/default_spec.rb
+++ test/integration/cernvm-fs/serverspec/localhost/default_spec.rb
@@ -4,7 +4,12 @@ describe package('cvmfs') do
   it { should be_installed }
 end
 
-describe file('/etc/auto.master') do
+describe file('/etc/auto.master.d/cvmfs.autofs') do
   it { should exist }
   it { should contain %r{^/cvmfs.*auto.cvmfs} }
+end
+
+describe file('/cvmfs') do
+  it { should exist }
+  it { should be_directory }
 end
