68dfd11798213c33b51eb7d1c7fee45e6dff9c0a
Christopher Huhn <C.Huhn@gsi.de>
2021-10-11 11:14:21 +0200

    Adapt and extend intergration tests

--- test/integration/cernvm-fs/serverspec/localhost/default_spec.rb
+++ test/integration/cernvm-fs/serverspec/localhost/default_spec.rb
@@ -4,7 +4,12 @@ describe package('cvmfs-client') do
   it { should be_installed }
 end
 
-describe file('/etc/auto.master') do
+describe file('/etc/auto.master.d/cvmfs.autofs') do
   it { should exist }
   it { should contain %r{^/cvmfs.*auto.cvmfs} }
+end
+
+describe command('automount -m') do
+  its(:exit_status) { should be_zero }
+  its(:stdout) { should contain 'Mount point: /cvmfs' }
 end
