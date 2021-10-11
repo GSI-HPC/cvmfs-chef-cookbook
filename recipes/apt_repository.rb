c00faba83d63d34d0fa8aa5b21280c8ab5ba0f9a
Christopher Huhn <C.Huhn@gsi.de>
2021-10-11 12:31:13 +0200

    HTTPS for CERN package repo

--- recipes/apt_repository.rb
+++ recipes/apt_repository.rb
@@ -1,11 +1,33 @@
 #
+# Cookbook Name:: cernvm-fs
+# Recipe:: apt_repository
 #
+# Copyright 2020-2021 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH
+#
+# Authors:
+#  Christopher Huhn   <C.Huhn@gsi.de>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+# Set up the CERN CVFMS package repository
 #
 
 package 'gnupg'
+package 'apt-transport-https'
 
 apt_repository 'cvmfs' do
-  uri          'http://cvmrepo.web.cern.ch/cvmrepo/apt'
+  uri          'https://cvmrepo.web.cern.ch/cvmrepo/apt'
   distribution node['lsb']['codename'] + '-prod'
   components   %w[main]
   key          'cvmfs-repo.asc'
