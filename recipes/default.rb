2df2ad5a27df92544e099849016b9005764cdda3
Christopher Huhn <C.Huhn@gsi.de>
2021-11-02 20:54:23 +0100

    Keep the previous behaviour and package naming

--- recipes/default.rb
+++ recipes/default.rb
@@ -25,10 +25,22 @@
 #
 
 # Install from source if a specific version is defined
-unless node[:cvmfs][:version].empty?
+unless node['cvmfs']['version'].empty?
   include_recipe 'cernvm-fs::install'
 else
-  package 'cvmfs'
+  # Otherwise it is assumed a package can be installed
+  case node['platform_version'].to_i
+  when 7
+    if node['cvmfs']['server']['repos'].empty?
+      package 'cvmfs-client'
+    else
+      package 'cvmfs'
+    end
+  when 8,9
+    package 'cvmfs-client'
+  else
+    package 'cvmfs'
+  end
 end
 
 # Deploy files to the /etc/cvmfs/keys directory
@@ -36,14 +48,14 @@ directory '/etc/cvmfs/keys' do
   recursive true
 end
 
-node[:cvmfs][:keys].each do |name,key|
+node['cvmfs']['keys'].each do |name, key|
   file "/etc/cvmfs/keys/#{name}" do
     mode "0644"
     content key.gsub(/^ */,'')
   end
 end
 
-if node[:cvmfs][:server][:repos].empty?
+if node['cvmfs']['server']['repos'].empty?
   include_recipe 'cernvm-fs::client'
 else
   include_recipe 'cernvm-fs::server'
